9989c1264dca519872340919546aa5a516952a5a
amatov <6277757+amatov@users.noreply.github.com>
2023-10-17 20:34:55 -0700

    Add files via upload

---
+++ listNOCO.m
@@ -0,0 +1,111 @@
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_0s_01\images\'); (1.5/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_0s_05\images\'); (1.5/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_0s_09\images\'); (2/3.5)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_0s_13\images\'); (2/2)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_0s_17\images\'); (1.5/6)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_0s_01\images\'); (1.25/3)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_0s_05\images\'); (1.5/1.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_0s_09\images\'); (2/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_0s_13\images\'); (2.75/2.25)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_0s_17\images\'); (2.25/3)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_0s_01\images\'); (1.5/3)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_0s_05\images\'); (1.1/3.75)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_0s_09\images\'); (2/3)
+ 
+% 'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_20s_02\images\'); 
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_20s_06\images\'); (2.5/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_20s_10\images\'); (3/4)
+% 'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_20s_14\images\'); 
+% 'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_20s_18\images\'); 
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_20s_02\images\'); (1.6/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_20s_06\images\'); (1.9/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_20s_10\images\'); (1.5/2.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_20s_14\images\'); (2.5/3.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_20s_18\images\'); (2/3.5)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_20s_02\images\'); (2.5/2.5)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_20s_06\images\'); (1.55/2)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_20s_10\images\'); (2/2.8)
+ 
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_300s_03\images\'); (1.5/3) % noisy, few feats
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_300s_07\images\'); (2.5/4) % a few
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_300s_11\images\'); (2.2/3)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_300s_15\images\'); (2.25/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_300s_19\images\'); (2/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_300s_03\images\'); (2/4) % very few
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_300s_07\images\'); (2/4) % very few
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_300s_11\images\'); (2/4) % no features
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_300s_15\images\'); (2/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_300s_19\images\'); (2/3.5)
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_300s_03\images\'); 
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_300s_07\images\'); (1.75/2)
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_300s_11\iages\'); 
+ 
+% 'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_600s_04\images\');
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_600s_08\images\'); (3/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_600s_12\images\'); (2.7/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_600s_16\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_par_600s_20\images\'); (2.75/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_600s_04\images\'); (2.2/5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_600s_08\images\'); % no features
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_600s_12\images\'); % no feats
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_600s_16\images\'); (2.5/3)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_par_600s_20\images\'); (2/4)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_600s_04\images\'); (2.25/3.5)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_600s_08\images\'); % too noisy (2/4)
+'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_par_600s_12\images\'); (2.5/4) % a few
+  
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_0s_01\images\'); (2/4.5)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_0s_05\images\'); (1.7/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_0s_09\images\'); (1.5/2)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_0s_13\images\'); (1.6/2)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_0s_17\images\'); (2.2/3)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_0s_01\images\'); (2/2) % strong bkgr
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_0s_05\images\'); (1.2/3.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_0s_09\images\'); (2.25/3.5)
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_0s_13\images\');
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_0s_17\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_0s_01\images\'); 
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_0s_05\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_0s_09\images\');
+
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_20s_02\images\'); (2.2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_20s_06\images\'); (1.5/3)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_20s_10\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_20s_14\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_20s_18\images\'); (2/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_20s_02\images\'); (3/2)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_20s_06\images\'); (2.5/3.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_20s_10\images\'); (2.6/4)
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_20s_14\images\');
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_20s_18\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_20s_02\images\'); 
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_20s_06\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_20s_10\images\');
+ 
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_300s_03\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_300s_07\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_300s_11\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_300s_15\images\'); (2.5/3.5) %very few
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_300s_19\images\'); (2.5/1.5) %deep field of view
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_300s_03\images\'); (3/2)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_300s_07\images\'); (2/4)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_300s_11\images\'); (2.5/4) % noisy
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_300s_15\images\');
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_300s_19\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_300s_03\images\'); 
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_300s_07\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_300s_11\images\');
+ 
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_600s_04\images\'); (2.2/4) % noisy, few feat's
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_600s_08\images\'); (2/4)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_600s_12\images\'); (2.25/4) % very few
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_600s_16\images\'); (2.5/3)
+'X:\AlexData11\NOCO\070816_RCC-4_Noco40nM_tiff\070816_RCC-4_VHL30_600s_20\images\'); (2/2) % very few
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_600s_04\images\'); (2.5/2.5)
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_600s_08\images\'); (2.4/4) 
+'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_600s_12\images\'); (2.75/4) % very few
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_600s_16\images\');
+% 'X:\AlexData11\NOCO\070818_RCC-4_Noco40nM_tiff\070818_RCC-4_VHL30_600s_20\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_600s_04\images\'); 
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_600s_08\images\');
+% 'X:\AlexData11\NOCO\070819_RCC-4_Noco40nM_tiff\070819_RCC-4_VHL30_600s_12\images\');
